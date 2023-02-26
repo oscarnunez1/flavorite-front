52b06154fd18e964b1f90c992b9e48e77f2fe480
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-26 18:37:01 -0500

    write index function for meal service

---
+++ src/services/mealService.ts
@@ -0,0 +1,22 @@
+// services
+import * as tokenService from './tokenService'
+
+// types
+import { Meal } from "../types/models"
+
+
+const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/meals`
+
+async function index(): Promise<Meal[]> {
+  try {
+    const res = await fetch(BASE_URL, {
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
+    })
+    return await res.json()
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export { index }
