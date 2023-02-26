0a3b5b1d6ce480c4718faafa4a80671e89dcf5ac
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-26 18:00:25 -0500

    stub up MealList.tsx

--- src/pages/MealList/MealList.tsx
+++ src/pages/MealList/MealList.tsx
@@ -1,1 +1,16 @@
-import './MealList.css'
+import styles from './MealList.module.css';
+
+interface MealListProps {
+  // Define the type of the props object
+}
+
+const MealList = (props: MealListProps): JSX.Element => {
+  return (
+    <div className={styles.container}>
+      Meal List
+    </div>
+  );
+}
+
+export default MealList;
+
