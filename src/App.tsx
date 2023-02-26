552a44b5a0d721b35b96a3f566b1dd401c8e7fc5
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-26 18:22:55 -0500

    place Meals on Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Login from './pages/Login/Login'
 import Landing from './pages/Landing/Landing'
 import Profiles from './pages/Profiles/Profiles'
 import ChangePassword from './pages/ChangePassword/ChangePassword'
+import MealList from './pages/MealList/MealList'
 
 // components
 import NavBar from './components/NavBar/NavBar'
@@ -21,7 +22,6 @@ import './App.css'
 
 // types
 import { User } from './types/models'
-import MealList from './pages/MealList/MealList'
 
 function App(): JSX.Element {
   const navigate = useNavigate()
