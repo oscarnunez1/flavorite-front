f1ec5632dda545b2eb070399507a75402531b988
Oscar Nunez <oscar.nunezcaba@gmail.com>
2023-02-26 19:34:57 -0500

    map through meals in MealList

--- src/pages/MealList/MealList.tsx
+++ src/pages/MealList/MealList.tsx
@@ -1,19 +1,20 @@
+import { Meal } from "../../types/models";
 import styles from './MealList.module.css';
-import { Meal } from '../../types/models';
 
 interface MealListProps {
   meals: Meal[];
 }
 
-const MealList = (props: MealListProps): JSX.Element => {
-  console.log("MealList props:", props);
-  
+const MealList = ({ meals }: MealListProps): JSX.Element => {
   return (
     <div className={styles.container}>
-      Meal List
+      {meals.map((meal) => (
+        <p key={meal.id}>
+          {meal.name}
+        </p>
+      ))}
     </div>
   );
-}
+};
 
 export default MealList;
-
